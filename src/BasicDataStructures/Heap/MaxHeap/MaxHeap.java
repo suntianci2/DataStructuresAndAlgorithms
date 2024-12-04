d04108005b2c7a1e0e9276221f0a617350769d3f
suntianci <371009757@qq.com>
2024-12-04 17:34:36 +0800

    数据结构与算法

--- src/BasicDataStructures/Heap/MaxHeap/MaxHeap.java
+++ src/BasicDataStructures/Heap/MaxHeap/MaxHeap.java
@@ -1,5 +1,7 @@
 package BasicDataStructures.Heap.MaxHeap;
 
+import java.util.StringJoiner;
+
 // 大顶堆
 public class MaxHeap {
     int size = 0;
@@ -7,12 +9,21 @@ public class MaxHeap {
     int[] array;
 
     public MaxHeap(int capacity) {
-        array = new int[capacity];
+        this.array = new int[capacity];
     }
 
     public MaxHeap(int[] array) {
         this.array = array;
         this.size = array.length;
+        heapify();
+    }
+
+    public MaxHeap(int[] array, int capacity) {
+        this.array = new int[capacity];
+        for (int i = 0; i < array.length; i++) {
+            this.array[i] = array[i];
+            this.size++;
+        }
         heapify();
     }
 
@@ -22,6 +33,55 @@ public class MaxHeap {
         for (int i = size / 2 - 1; i >= 0; i--) {
             down(i);
         }
+    }
+
+    // 获得堆顶元素，但不取出
+    public int peek() {
+        if (isEmpty()) {
+            throw new NullPointerException();
+        }
+        return array[0];
+    }
+
+    // 取出堆顶元素
+    public int poll() {
+        return poll(0);
+    }
+
+    // 删除指定索引的元素
+    public int poll(int index) {
+        if (index >= size) {
+            throw new NullPointerException();
+        }
+        // 直接取出效率低，因为需要移动堆中删除的索引后面的元素。
+        // 先和堆底元素交换
+        swap(index, size - 1);
+        // 然后移除堆底元素
+        int value = array[--size];
+        // 然后对索引处的元素执行下潜操作
+        down(index);
+        return value;
+    }
+
+    // 替换堆顶元素
+    public int replace(int newValue) {
+        if (isEmpty()) {
+            throw new NullPointerException();
+        }
+        int oldValue = array[0];
+        array[0] = newValue;
+        down(0);
+        return oldValue;
+    }
+
+    // 向堆尾部添加元素
+    public boolean offer(int value) {
+        if (isFull()) {
+            return false;
+        }
+        array[size++] = value;
+        up(size - 1);
+        return true;
     }
 
     // 下潜操作
@@ -39,16 +99,55 @@ public class MaxHeap {
             maxIndex = rIndex;
         }
         // 表示可以下潜，左右孩子中存在比当前元素还大的值
-        if (maxIndex != index){
+        if (maxIndex != index) {
             swap(index, maxIndex);
             // 递归执行下潜操作
             down(maxIndex);
         }
     }
 
-    private void swap(int index1, int index2){
+    // 上浮操作
+    private void up(int index) {
+        // 找到当前元素的父节点索引
+        int parent = (index - 1) / 2;
+        // down方法是用递归实现的，这里用循环
+        // 存在父节点，则循环
+        while (parent >= 0) {
+            // 若当前节点元素值小于等于父节点，则循环停止
+            if (array[index] <= array[parent]) {
+                break;
+            }
+            swap(index, parent);
+            // 把父节点看作新节点，继续循环
+            index = parent;
+            // 找到新的节点的父节点索引
+            parent = (index - 1) / 2;
+        }
+    }
+
+    // 交换两个元素
+    private void swap(int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
+    }
+
+    // 判断堆是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 判断堆是否满
+    public boolean isFull() {
+        return size == array.length;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner sj = new StringJoiner(", ","[","]");
+        for (int i = 0; i < this.size; i++) {
+            sj.add(String.valueOf(this.array[i]));
+        }
+        return sj.toString();
     }
 }
--- src/BasicDataStructures/Heap/MaxHeap/MaxHeapTest.java
+++ src/BasicDataStructures/Heap/MaxHeap/MaxHeapTest.java
@@ -8,7 +8,27 @@ public class MaxHeapTest {
     @Test
     public void testMaxHeap() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        MaxHeap maxHeap = new MaxHeap(array);
-        System.out.println(Arrays.toString(maxHeap.array));
+        MaxHeap maxHeap = new MaxHeap(array,20);
+        System.out.println(maxHeap);
+
+        System.out.println("peek=====================");
+        System.out.println(maxHeap.peek());
+        System.out.println(maxHeap.peek());
+        System.out.println(maxHeap);
+        System.out.println("poll=====================");
+        System.out.println(maxHeap.poll());
+        System.out.println(maxHeap.poll());
+        System.out.println(maxHeap);
+        System.out.println("poll(value)=====================");
+        System.out.println(maxHeap.poll(4));
+        System.out.println(maxHeap);
+        System.out.println("replace=====================");
+        System.out.println(maxHeap.replace(10));
+        System.out.println(maxHeap);
+        System.out.println("offer=====================");
+        maxHeap.offer(16);
+        maxHeap.offer(0);
+        System.out.println(maxHeap);
+
     }
 }
